94531fe9714913b3319771657ba7890ab6df1411
Aria Gachkar <Ariagachkar78@gmail.com>
2024-11-02 20:24:07 +0330

    change post list and post details from function to class

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.views import generic
 
 from posts.models import Post, Comment
 from .forms import PostForm
@@ -13,19 +14,26 @@ def home(request):
     return HttpResponse('<h3>Welcome to my Blog...</h3>')
 
 
-def post_list(request):
+ #def post_list(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'posts/post_list.html', context=context)
 
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+
 
 def post_detail(request, post_id):
-    post = Post.objects.get(pk=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     comment = Comment.objects.filter(post=post)
     context = {'post': post, 'comment': comment}
     return render(request, 'posts/post_detail.html', context=context)
 
-
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
 def post_create(request):
     if request.method == "POST":
         form = PostForm(request.POST)

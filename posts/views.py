85e60d5262e1994225f4b1f1bb4239caa5c059a7
Aria Gachkar <Ariagachkar78@gmail.com>
2024-11-09 20:24:10 +0330

    add CRUD to post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -22,10 +24,27 @@ class PostListView(APIView):
 
 
 class PostDetailView(APIView):
-    def get(self, request, pk):
+    def get_object(self, pk):
         try:
             post = Post.objects.get(pk=pk)
         except Post.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            raise Http404
+        return post
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
         serializer = PostSerializer(post)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
